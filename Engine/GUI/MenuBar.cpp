4316e470303d2ff6125db2aae8f07dd376ffce0f
YlaxusB <souzagiovane2019@gmail.com>
2023-07-23 07:11:45 -0300

    Minor adjustments to Title Bar

--- Engine/GUI/MenuBar.cpp
+++ Engine/GUI/MenuBar.cpp
@@ -1,4 +1,4 @@
-#include "Engine/Core/PreCompiledHeaders.h"
+﻿#include "Engine/Core/PreCompiledHeaders.h"
 #include "MenuBar.h"
 #include "Engine/Application/FileDialog/FileDialog.h"
 #include "Engine/Application/Serializer/SceneSerializer.h"
@@ -21,21 +21,21 @@ namespace Engine {
 
 
 				// Create an invisible margin to align the buttons to the right
-				float spacingX = ImGui::GetWindowSize().x - ImGui::GetCursorPosX() - ImGui::GetStyle().ItemSpacing.x * 3;
-				spacingX -= 300;
+				float spacingX = Application->appSizes->appSize.x - 150;
 				ImGui::Dummy(ImVec2(spacingX, 0));
 
 				// Create the right corner buttons
 				ImGui::SameLine();
-				if (ImGui::Button("Minimize")) {
+				ImVec2 buttonSize = ImVec2(25, 25);
+				if (ImGui::Button("-", buttonSize)) {
 					glfwIconifyWindow(Application->Window->glfwWindow);
 				}
 				ImGui::SameLine();
-				if (ImGui::Button("Maximize")) {
+				if (ImGui::Button("□", buttonSize)) {
 					glfwMaximizeWindow(Application->Window->glfwWindow);
 				}
 				ImGui::SameLine();
-				if (ImGui::Button("Close")) {
+				if (ImGui::Button("X", buttonSize)) {
 					glfwSetWindowShouldClose(Application->Window->glfwWindow, true);
 				}
 
